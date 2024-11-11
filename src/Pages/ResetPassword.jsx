dca7741d893612f06e9183f6a21d9a0552dd34d4
imac141828 <imac141828@imac141828s-iMac.local>
2024-11-11 11:47:57 +0100

    arranging frontend googleauth

--- src/Pages/ResetPassword.jsx
+++ src/Pages/ResetPassword.jsx
@@ -1,37 +1,40 @@
 import { useState } from 'react';
 import axios from 'axios';
 import { API_URL } from '../../constants';
+import { toast } from 'sonner';
 
 const PasswordResetRequest = () => {
   const [email, setEmail] = useState('');
-  const [message, setMessage] = useState('');
 
   const handleSubmit = async (e) => {
     e.preventDefault();
 
     try {
       const response = await axios.post(`${API_URL}/users/reset-password-request`, { email });
-      setMessage(response.data.msg);
+      
+      toast.success("Password reset email sent successfully.");
     } catch (error) {
-      setMessage(error.response?.data?.msg || 'An error occurred');
+      toast.error("Failed to send password reset email. Please try again.");
     }
   };
 
   return (
-    <div className='h-screen bg-green w-full bg-neutral-900 flex item-center justify-center'>
-      <form onSubmit={handleSubmit} className='flex item-center justify-center flex-col gap-4'>
-        <div className='flex item-center justify-center flex-col '>
-          <label className='text-2xl text-white'>Email</label>
+    <div className="h-screen bg-green w-full bg-neutral-900 flex items-center justify-center">
+      <form onSubmit={handleSubmit} className="flex items-center justify-center flex-col gap-4">
+        <div className="flex items-center justify-center flex-col">
+          <label className="text-2xl text-white">Email</label>
           <input
             type="email"
             value={email}
             onChange={(e) => setEmail(e.target.value)}
             required
-           className='h-9 rounded-md outline-none p4 w-96 text-xl text-black'/>
+            className="h-9 rounded-md outline-none p-4 w-96 text-xl text-black"
+          />
         </div>
-        <button type="submit" className='bg-red-700 w-96 h-9 rounded-md'>Reset password</button>
+        <button type="submit" className="bg-red-700 w-96 h-9 rounded-md">
+          Reset Password
+        </button>
       </form>
-      {message && <p>{message}</p>}
     </div>
   );
 };
